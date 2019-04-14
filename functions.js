95e305f88a2626dab13c035b8c9f272ea9e29a83
Minh <minhdotcom@gmail.com>
2019-04-14 23:45:31 +0700

    done

--- functions.js
+++ functions.js
@@ -1,29 +1,32 @@
-var amount = Number(document.getElementById("amount").value);
-var fromCurrency = document.getElementById("fromCurrency").value;
-var toCurrency = document.getElementById("toCurrency").value;
+var amount;
+var fromCurrency;
+var toCurrency;
 var convertResult;
 console.log(amount);
 console.log(fromCurrency);
 console.log(toCurrency);
 
 function convert_currency () {
+    amount = Number(document.getElementById("amount").value);
+    fromCurrency = document.getElementById("fromCurrency").value;
+    toCurrency = document.getElementById("toCurrency").value;
     if (fromCurrency == "" || toCurrency == "") {
         convertResult = "Please select 'from currency' and 'to currency' before calculating";
-    } else if (fromCurrency === "VND") {
+    } else if (fromCurrency == "VND") {
         switch (toCurrency) {
-            case 1: "USD"
+            case "USD":
                 convertResult = amount / 23;
                 break;
-            case 2: "VND"
+            case "VND":
                 convertResult = amount;
                 break;
         };
-    } else if (fromCurrency === "USD") {
+    } else if (fromCurrency == "USD") {
         switch (toCurrency) {
-            case 1: "VND"
+            case "VND":
                 convertResult = amount * 23;
                 break;
-            case 2: "USD"
+            case "USD":
                 convertResult = amount;
                 break;
         };
